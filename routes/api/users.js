c87839d83ec3f10526f97a84f1bf3f077b84961e
sultan ahmed <sultan7.syl@gmail.com>
2022-01-06 00:59:42 +0600

    user model & user record save in db

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,62 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
 
-//@route get /api/users
-//@desc test
+//@route Post /api/users
+//@desc User Registraion
 //@access public
 
-router.get('/',(req,res)=> res.send("user"));
+router.post(
+  "/",
+  [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "Email is required").not().isEmpty(),
+    check("email", "type a valid email").isEmail(),
+    check("password", "Password is required").not().isEmpty(),
+    check("password", "Password must have 6 char").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // console.log(req.body);
+    try {
+      const { name, email, password } = req.body;
+      // if user exsists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({
+          errors: [{ msg: "User already exists with this email" }],
+        });
+      }
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        password,
+        avatar,
+      });
+      // Encrypt password with bcryptjs
+      const salt = await bcrypt.genSaltSync(10);
+      user.password = await bcrypt.hash(password, salt);
+      //   await user.save();
+      res.send("User Registration Sussesfully");
+      // jwt token
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const PORT = process.env.PORT || 5000;
 const connectDb = require("./config/db");
 connectDb();
 
-app.get("/",(req,res)=>res.send("app runing"));
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("app runing"));
 
 //define routs
-app.use("/api/users",require("./routes/api/users"));
-app.use('/api/auth',require("./routes/api/auth"));
-app.use('/api/profile',require("./routes/api/profile"));
-app.use('/api/post',require("./routes/api/post"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
 
-app.listen(PORT,()=> console.log(`server running at ${PORT}`));
+app.listen(PORT, () => console.log(`server running at ${PORT}`));
