5c0a1cfa4a2e3dcadaf79805b38f9c3fa5363056
sultan ahmed <sultan7.syl@gmail.com>
2022-01-10 04:05:26 +0600

    Jwt token verify ,use middleware and user login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,74 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
+const auth = require("../../middleware/auth");
+const res = require("express/lib/response");
 
-//@route get /api/users
-//@desc test
+//@route Post /api/users
+//@desc User Registraion
 //@access public
 
-router.get('/',(req,res)=> res.send("auth"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    onsole.error(err.message);
+    res.status(500).send("Internal server error");
+  }
+});
+
+router.post(
+  "/",
+  [
+    check("email", "Email is required").not().isEmpty(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // console.log(req.body);
+    try {
+      const { email, password } = req.body;
+      // if user exsists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({
+          errors: [{ msg: "User Not Found!" }],
+        });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({
+          errors: [{ msg: "Password is not match" }],
+        });
+      }
+
+      // jwt token
+      const payload = {
+        user: { id: user.id },
+      };
+      jwt.sign(
+        payload,
+        config.get("secretToken"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 
@@ -49,9 +51,20 @@ router.post(
       // Encrypt password with bcryptjs
       const salt = await bcrypt.genSaltSync(10);
       user.password = await bcrypt.hash(password, salt);
-      //   await user.save();
-      res.send("User Registration Sussesfully");
+      await user.save();
       // jwt token
+      const payload = {
+        user: { id: user.id },
+      };
+      jwt.sign(
+        payload,
+        config.get("secretToken"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Internal server error");
