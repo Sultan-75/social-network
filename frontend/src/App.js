53f819f0e5816f6777d04c8424602c704272dbca
sultan ahmed <sultan7.syl@gmail.com>
2022-02-23 01:02:21 +0600

    get profile by redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,39 +2,50 @@ import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Dashboard from "./components/dashboard/Dashboard";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import setAuthToken from "./utils/setAuthToken";
 import { loadUser } from "./actions/auth";
+import PrivateRoute from "./routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
-
+// style files
 import "./App.css";
 
 if (localStorage.token) {
-	setAuthToken(localStorage.token);
+  setAuthToken(localStorage.token);
 }
 
 const App = () => {
-	useEffect(() => {
-		store.dispatch(loadUser());
-	}, []);
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
 
-	return (
-		<Provider store={store}>
-			<BrowserRouter>
-				<Navbar />
-				<Alert />
-				<Routes>
-					<Route exact path="/" element={<Landing />} />
-					<Route exact path="/register" element={<Register />} />
-					<Route exact path="/login" element={<Login />} />
-				</Routes>
-			</BrowserRouter>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <Alert />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route
+            exact
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 };
 
 export default App;
