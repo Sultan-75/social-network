6df709eee6062f1230583fbe7ceccb2aa1279a7f
sultan ahmed <sultan7.syl@gmail.com>
2022-03-08 20:30:40 +0600

    get single profile by id with redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,23 @@ import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-import Dashboard from "./components/dashboard/Dashboard";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import setAuthToken from "./utils/setAuthToken";
 import { loadUser } from "./actions/auth";
+import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./routing/PrivateRoute";
-import EditProfile from "./components/profile-forms/EditProfile";
 import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
-import Profile from "./components/profiles/Profile";
+import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
-// style files
+
 import "./App.css";
 
 if (localStorage.token) {
@@ -38,7 +39,8 @@ const App = () => {
           <Route exact path="/" element={<Landing />} />
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/login" element={<Login />} />
-          <Route exact path="/profiles" element={<Profile />} />
+          <Route exact path="/profiles" element={<Profiles />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
           <Route
             exact
             path="/dashboard"
