cd650599ca9fedae4c319d1b6c770e277b920538
sultan ahmed <sultan7.syl@gmail.com>
2022-04-24 01:57:40 +0600

    bug fixs

--- frontend/src/components/post/CommentItem.js
+++ frontend/src/components/post/CommentItem.js
@@ -5,28 +5,38 @@ import { connect } from "react-redux";
 import { deleteComment } from "../../actions/post";
 
 const CommentItem = ({
+  auth,
   deleteComment,
   comment: { _id, user, name, avatar, text, date },
   postId,
 }) => {
   return (
     <div className="post bg-white p-1 my-1">
-      <div>
+      <div className="post-img">
         <Link to={`/profile/${user}`}>
           <img className="round-img" src={avatar} alt="" />
           <h4>{name}</h4>
         </Link>
       </div>
-      <div>
-        <p className="my-1">{text}</p>
+      <div className="post-content">
+        <p className="my-1 comment-text">{text}</p>
         <p className="post-date">
-          Posted on
-          <Moment format="YYYY/MM/DD">{date}</Moment>
+          Commented on <Moment format="YYYY/MM/DD">{date}</Moment>
         </p>
+        {user === auth.user._id && (
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={() => deleteComment(postId, _id)}
+          >
+            <i className="fas fa-times"></i>
+          </button>
+        )}
       </div>
-      <button onClick={() => deleteComment(postId, _id)}>Delete</button>
     </div>
   );
 };
-
-export default connect(null, { deleteComment })(CommentItem);
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+export default connect(mapStateToProps, { deleteComment })(CommentItem);
--- frontend/src/components/post/PostItem.js
+++ frontend/src/components/post/PostItem.js
@@ -8,21 +8,16 @@ const PostItem = ({
 }) => {
   return (
     <div className="post bg-white p-1 my-1">
-      <div className="post-div-1">
+      <div className="post-img">
         <Link to={`/profile/${user}`}>
           <img className="round-img" src={avatar} alt="" />
           <h4>{name}</h4>
         </Link>
       </div>
-      <div className="post-div-2">
+      <div className="post-content">
         <h3 className="my-1">{title}</h3>
-        <img
-          // src={image}
-          src={generatePublicUrl(image)}
-          alt="post-image"
-          style={{ width: "100%", height: "70%" }}
-        />
         <p className="my-1">{text}</p>
+        <img className="comtent-img" src={generatePublicUrl(image)} alt="" />
         <p className="post-date">
           Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
         </p>
